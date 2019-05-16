eed1e7dfc2d84e40bfc0bf09759914c7abd93398
Victoria Lee <vickyloon91@gmail.com>
2019-05-16 15:24:33 +0800

    added turn on and turn off button

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'developer.chrome.com' },
+        pageUrl: { hostEquals: 'www.google.com' },
       })
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
