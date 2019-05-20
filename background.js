e38025e5320e1f14d1a7ee95af23c2054be0ae26
Victoria Lee <vickyloon91@gmail.com>
2019-05-20 23:00:40 +0800

    added Matt's comments

--- background.js
+++ background.js
@@ -14,6 +14,8 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
+//Matt says have to build backend as an API then make an API call using fetxh.
+//return a JWT to store in browser
 // Basic fetch request to fetch a JSON file and print it to the console.
 //fetch() takes one argument, returns a promise containing the response, json() extracts the JSON content
 fetch('http://google.com/images.json')
