d7f917285a550253dc5f5baaf7eec7aaa6e00840
edmond <tanjinyuan5@gmail.com>
2019-05-22 14:28:01 +0800

    changes to popup.js

--- popup.js
+++ popup.js
@@ -8,7 +8,22 @@
 //     )
 //   })
 // }
+let enabled = true;
+const myButton = document.getElementById('toggle');
 
+chrome.storage.local.get('enabled', data => {
+  console.log(data.enabled,'get')
+    enabled = !!data.enabled;
+    myButton.checked = enabled ? true : false;
+});
+
+myButton.onclick = () => {
+  enabled = !enabled;
+  console.log(enabled,'click')
+  chrome.tabs.reload()
+    // myButton.checked = enabled ? true : false;
+    chrome.storage.local.set({enabled:enabled});
+};
 document.querySelector('#go-to-options').addEventListener('click',function() {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
@@ -18,19 +33,3 @@ document.querySelector('#go-to-options').addEventListener('click',function() {
 });
 
 
-var enabled = true;
-var myButton = document.getElementById('toggle');
-
-chrome.storage.local.get('enabled', data => {
-  console.log(data.enabled,'get')
-    enabled = !!data.enabled;
-    myButton.checked = enabled ? true : false;
-});
-
-myButton.onclick = () => {
-  console.log(enabled,'click')
-    enabled = !enabled;
-    chrome.tabs.reload()
-    // myButton.checked = enabled ? true : false;
-    chrome.storage.local.set({enabled:enabled});
-};
