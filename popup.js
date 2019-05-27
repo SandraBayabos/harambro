f472555fabb3c87bf390467cd8fc428dfe2d5ae5
Hayden <Codecode272727@gmail.com>
2019-05-27 17:35:21 +0800

    create setting button

--- popup.js
+++ popup.js
@@ -8,26 +8,71 @@
 //     )
 //   })
 // }
+let title = document.createElement("div")
+title.innerHTML = "Harambro"
+
+let toggle = document.createElement('input');
+
+let button = document.createElement('button')
+var image = document.createElement('IMG')
+image.setAttribute('src', 'images/setting(1).png')
+button.appendChild(image)
+
+
+let header = document.querySelector('header')
+
+let email = document.createElement("label")
+email.innerHTML = "Email     :"
 
 //Create login form in popup for use on first time
 let form = document.createElement("form");
 form.setAttribute('method', "post");
 form.setAttribute('action', "submit.php");
-
 let input = document.createElement("input"); //input element, text
 input.setAttribute('type', "text");
-input.setAttribute('name', "username");
+input.setAttribute('name', "email");
+
+let password = document.createElement("label")
+password.innerHTML = "Password:"
+
+
+let input1 = document.createElement("input"); //input element, text
+input1.setAttribute('type', "text");
+input1.setAttribute('name', "password");
 
 let submit = document.createElement("input"); //input element, Submit button
 submit.setAttribute('type', "submit");
 submit.setAttribute('value', "Submit");
 
+// header.appendChild(button)
+header.appendChild(button)
+header.appendChild(title)
+form.appendChild(email)
 form.appendChild(input);
+form.appendChild(password);
+form.appendChild(input1);
 form.appendChild(submit);
 
 document.getElementsByTagName('body')[0].appendChild(form);
 
-document.querySelector('#go-to-options').addEventListener('click', function () {
+let body = document.querySelector('body')
+title.style.fontSize = "28px"
+body.style.width = "300px"
+body.style.margin = "15px"
+body.style.display = 'flex'
+body.style.justifyContent = 'center'
+body.style.flexDirection = 'column'
+body.classList.add('container')
+button.classList.add('setting')
+header.style.alignItems = 'left'
+button.classList.add('go-to-options')
+button.style.width = '25px'
+button.style.height = "25px"
+button.style.alignItems = 'right'
+image.style.width = '25px'
+image.style.height = '25px'
+
+document.querySelector('.go-to-options').addEventListener('click', function () {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
