d79f185e4c5eb65f34049f781e9184122cfaa73f
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-31 10:48:41 +0800

    changed shit

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-let items = ["sugar", "fuck", "sex", "shit", "asshole", "porn", "bitch", "momo", "pussy", "cunt", "nigger"]
+let items = ["sugar", "fudge", "banana", "crap", "porn", "happy tree friends", "momo"]
 chrome.storage.sync.set({ blackListItem: items })
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: '#3aa757' }, function () {
